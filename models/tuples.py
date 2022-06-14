8c8d6bea137216049ee6ec968f6f95db8abe27c0
Chris Preston <Chris_Preston@waters.com>
2022-06-14 17:50:25 +0100

    Refactored basic tuples

--- models/tuples.py
+++ models/tuples.py
@@ -1,18 +1,56 @@
-def tuple(x, y, z, w):
-    return {'x': float(x), 'y': float(y), 'z': float(z), 'w': float(w)}
+class Tuple:
+    def __init__(self, x=0, y=0, z=0, w=0):
+        self.x = float(x)
+        self.y = float(y)
+        self.z = float(z)
+        self.w = float(w)
+
+    def __repr__(self) -> str:
+        return f'Tuple({self.x!r}, {self.y!r}, {self.z!r}, {self.w!r})'
+
+    def __eq__(self, other):
+        def eqval(m, n):
+            EPSILON = 1e-6
+            return abs(m - n) < EPSILON
+
+        if isinstance(other, Tuple):
+            return (
+                eqval(self.x, other.x)
+                and eqval(self.y, other.y)
+                and eqval(self.z, other.z)
+                and eqval(self.w, other.w)
+            )
+        else:
+            return NotImplemented
 
 
-def point(x, y, z):
-    return tuple(x, y, z, 1.0)
+class Point:
+    def __init__(self, x=0, y=0, z=0):
+        self.p = Tuple(x, y, z, 1.0)
+
+    def __repr__(self) -> str:
+        return f'Point({self.p.x!r}, {self.p.y!r}, {self.p.z!r})'
+
+    def __eq__(self, other):
+        if isinstance(other, Point):
+            return self.p == other.p
+        elif isinstance(other, Tuple):
+            return self.p == other
+        else:
+            return NotImplemented
 
 
-def vector(x, y, z):
-    return tuple(x, y, z, 0.0)
+class Vector:
+    def __init__(self, x=0, y=0, z=0):
+        self.v = Tuple(x, y, z, 0.0)
 
+    def __repr__(self) -> str:
+        return f'Vector({self.v.x!r}, {self.v.y!r}, {self.v.z!r})'
 
-def equals(a, b):
-    def eqval(m, n):
-        EPSILON = 1e-6
-        return abs(m - n) < EPSILON
-
-    return eqval(a['x'], b['x']) and eqval(a['y'], b['y']) and eqval(a['z'], b['z']) and eqval(a['w'], b['w'])
+    def __eq__(self, other):
+        if isinstance(other, Vector):
+            return self.v == other.v
+        elif isinstance(other, Tuple):
+            return self.v == other
+        else:
+            return NotImplemented
